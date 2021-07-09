afe7f5485e3515ac1d50b89db8326e71f3e5af45
benjaminduffin <66788721+benjaminduffin@users.noreply.github.com>
2021-07-09 12:09:53 -0400

    Update test_script.R

--- test_script.R
+++ test_script.R
@@ -6,3 +6,9 @@ library(ggplot2)
 x <- seq(1, 10, 1)
 y <- x^2
 plot(x,y)
+
+# a first edit to the file 
+x <- data.frame(grp = rep(letters[1:10], 2), value = rep(1:10, 2))
+
+ggplot(x) + 
+geom_bar(aes(x = grp, y = value))
