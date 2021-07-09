420361e0b84aa99eef2f92d93a5991ed9311857b
BENDUFFIN <BENJAMIN.DUFFIN@NOAA.GOV>
2021-07-09 12:05:06 -0400

    my first git commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # logbook_scripts
+
+## This runs through some info regarding how to set up Git 
---
+++ test_script.R
@@ -0,0 +1,8 @@
+## GIT REPO TEST #
+# BD 7/9/2021 
+
+library(ggplot2) 
+
+x <- seq(1, 10, 1)
+y <- x^2
+plot(x,y)
